e47d4d193bb867754ad94f71241483e3cdd0a164
Trần Thanh Phong <143787214+blackjack-ttp@users.noreply.github.com>
2024-10-01 13:40:58 +0700

    add src demo 12: crack pw

---
+++ demo12.py
@@ -0,0 +1,19 @@
+from random import *
+import os
+
+u_pwd = input('Enter the password: ')
+pwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+       'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
+       '3', '4', '5', '6', '7', '8', '9']
+
+pw = ""
+
+while (pw != u_pwd):
+    pw = ""
+    for letter in range(len(u_pwd)):
+        guess_pwd = pwd[randint(0, 17)]
+        pw = str(guess_pwd)+str(pw)
+        print(pw)
+        print("Cracking password ... Please waitting")
+        os.system("clear")
+print("Your password is: ", pw)
