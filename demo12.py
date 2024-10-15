70dbabf756c8b3ce04b40109f63e7157faa05223
blackjack-ttp <phongtt120298@outlook.com>
2024-10-15 11:38:17 +0700

    edit src demo 12

--- demo12.py
+++ demo12.py
@@ -2,16 +2,29 @@ from random import *
 import os
 
 u_pwd = input('Enter the password: ')
-pwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-       'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
-       '3', '4', '5', '6', '7', '8', '9']
+# pwd = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
+#        'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2',
+#        '3', '4', '5', '6', '7', '8', '9']
+
+pwd = [
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+]
 
 pw = ""
 
 while (pw != u_pwd):
     pw = ""
     for letter in range(len(u_pwd)):
-        guess_pwd = pwd[randint(0, 17)]
+        guess_pwd = pwd[randint(0, 9)]
         pw = str(guess_pwd)+str(pw)
         print(pw)
         print("Cracking password ... Please waitting")
